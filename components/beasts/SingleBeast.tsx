c6b0166fce7cdf8263c08f27e0edfb24895306fe
samuel-bd <samuely@frisseblikken.com>
2023-11-23 12:37:03 +0700

    feat: init single beast page design

--- components/beasts/SingleBeast.tsx
+++ components/beasts/SingleBeast.tsx
@@ -44,8 +44,36 @@ export default function SingleBeast({ name }: { name: string }) {
   const fetchedData: Monster = data[0];
 
   return (
-    <h1>
-      <span>{fetchedData?.name}</span>
-    </h1>
+    <section className="w-full h-full flex flex-col gap-4 pb-6">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl md:text-6xl tracking-tight font-semibold">
+          {fetchedData.name}
+          {/* <span className="text-base md:text-lg opacity-50 font-normal tracking-normal">
+            #{fetchedData.beastid}
+          </span> */}
+        </h1>
+        <div className="flex gap-1">
+          <Badge>{fetchedData.type}</Badge>
+        </div>
+      </div>
+      <div>
+        <section className="w-full grid min-[470px]:grid-cols-2 md:grid-cols-3 gap-4">
+          <Card className="relative py-4">
+            <div className="w-full h-full min-h-[5rem] min-w-[5rem] grid items-center relative">
+              <CardImage
+                src={fetchedData.images!.animated!}
+                alt={fetchedData.name!}
+              />
+            </div>
+          </Card>
+          <Card className="md:col-span-2 md:row-span-2"></Card>
+          <Card>
+            <CardContent className="pt-6">
+              <p>{fetchedData.description}</p>
+            </CardContent>
+          </Card>
+        </section>
+      </div>
+    </section>
   );
 }
